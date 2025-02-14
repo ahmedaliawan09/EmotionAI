65f85f9944a8f99d6fd97bc334fadb936f85e481
Ahmed Ali <120272620+ahmedaliawan09@users.noreply.github.com>
2025-02-14 16:40:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") or st.secrets.get("GEMINI_API_KEY")
 # ✅ Check if API Key Exists
 if not GEMINI_API_KEY:
     #st.error("🚨 API Key Not Found! Please check your GitHub Secrets or environment variables.")
-else:
+  else:
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel("gemini-2.0-flash")
     #st.success("✅ API Key Loaded Secure")
